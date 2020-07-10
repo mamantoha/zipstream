2400684e6ac5531e7d254fb8a803e35af7ebab33
Anton Maminov <anton.linux@gmail.com>
2020-07-10 14:31:30 +0300

    extract tar methods into helper

---
+++ src/zipstream/helper.cr
@@ -0,0 +1,54 @@
+module Zipstream
+  module Helper
+    extend self
+
+    def tar_directory!(path : String, io)
+      Crystar::Writer.open(io) do |tar|
+        Dir[File.join(path, "**/*")].each do |entry|
+          next unless File.readable?(entry)
+
+          relative_path = entry.sub(path, "").lstrip("/")
+          permissions = File.info(entry).permissions.value.to_i64
+
+          if File.directory?(entry)
+            header = Crystar::Header.new(
+              name: "#{relative_path}/",
+              mode: permissions,
+              mod_time: File.info(entry).modification_time,
+              size: 0_i64
+            )
+
+            tar.write_header(header)
+            nil
+          else
+            header = Crystar::Header.new(
+              name: relative_path,
+              mode: permissions,
+              mod_time: File.info(entry).modification_time,
+              size: File.info(entry).size.to_i64
+            )
+
+            tar.write_header(header)
+            tar.write(File.read(entry).to_slice)
+          end
+        end
+      end
+    end
+
+    def tar_file!(path : String, io)
+      permissions = File.info(path).permissions.value.to_i64
+
+      Crystar::Writer.open(io) do |tar|
+        header = Crystar::Header.new(
+          name: File.basename(path),
+          mode: permissions,
+          mod_time: File.info(path).modification_time,
+          size: File.info(path).size.to_i64
+        )
+
+        tar.write_header(header)
+        tar.write(File.read(path).to_slice)
+      end
+    end
+  end
+end
--- src/zipstream/http/server/handlers/tgz_handler.cr
+++ src/zipstream/http/server/handlers/tgz_handler.cr
@@ -22,9 +22,9 @@ module Zipstream
       end
 
       if File.directory?(config.path)
-        tar_directory!(config.path, writer)
+        tgz_directory!(config.path, writer)
       else
-        tar_file!(config.path, writer)
+        tgz_file!(config.path, writer)
       end
 
       writer.close
@@ -34,56 +34,15 @@ module Zipstream
       call_next(context)
     end
 
-    private def tar_directory!(path : String, io : IO)
+    private def tgz_directory!(path : String, io : IO)
       Compress::Gzip::Writer.open(io) do |gzip|
-        Crystar::Writer.open(gzip) do |tar|
-          Dir[File.join(path, "**/*")].each do |entry|
-            next unless File.readable?(entry)
-
-            relative_path = entry.sub(Regex.escape(path), "")
-            permissions = File.info(entry).permissions.value.to_i64
-
-            if File.directory?(entry)
-              hdr = Crystar::Header.new(
-                name: "#{relative_path}/",
-                mode: permissions,
-                mod_time: File.info(entry).modification_time,
-                size: 0_i64
-              )
-
-              tar.write_header(hdr)
-              nil
-            else
-              hdr = Crystar::Header.new(
-                name: relative_path,
-                mode: permissions,
-                mod_time: File.info(entry).modification_time,
-                size: File.info(entry).size.to_i64
-              )
-
-              tar.write_header(hdr)
-              tar.write(File.read(entry).to_slice)
-            end
-          end
-        end
+        Zipstream::Helper.tar_directory!(path, gzip)
       end
     end
 
-    private def tar_file!(file : String, io : IO)
-      permissions = File.info(file).permissions.value.to_i64
-
+    private def tgz_file!(path : String, io : IO)
       Compress::Gzip::Writer.open(io) do |gzip|
-        Crystar::Writer.open(gzip) do |tar|
-          header = Crystar::Header.new(
-            name: File.basename(file),
-            mode: permissions,
-            mod_time: File.info(file).modification_time,
-            size: File.info(file).size.to_i64
-          )
-
-          tar.write_header(header)
-          tar.write(File.read(file).to_slice)
-        end
+        Zipstream::Helper.tar_file!(path, gzip)
       end
     end
   end
