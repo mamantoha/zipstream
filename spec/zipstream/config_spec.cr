87accc58689cf649f82966163f25a2d5e3b7033b
Anton Maminov <anton.linux@gmail.com>
2024-09-17 17:59:48 +0300

    fix spec on Linux/macOS

--- spec/zipstream/config_spec.cr
+++ spec/zipstream/config_spec.cr
@@ -4,8 +4,8 @@ describe Zipstream::Config do
   config = Zipstream::Config.new
 
   context "host" do
-    it "should has host" do
-      config.host.should eq("")
+    it "should has host as local IP address" do
+      config.host.should match(/\d+\.\d+\.\d+\.\d+/)
     end
   end
 end
